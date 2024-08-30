5edb57c1b03031912b28dc733b67825a2013a32f
kristianwahlroos <kristian.whl@gmail.com>
2024-08-30 15:46:21 +0300

    feat: add scale transformation for easier editing

---
+++ unity_shader_practice/Assets/Scripts/ScaleTransformation.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ScaleTransformation : Transformation
+{
+
+    public Vector3 scale;
+
+    public override Vector3 Apply(Vector3 point)
+    {
+        point.x *= scale.x;
+        point.y *= scale.y;
+        point.z *= scale.z;
+        return point;
+    }
+}
